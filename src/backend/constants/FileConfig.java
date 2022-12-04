28c6e20217372d80e6f319daabfdd98daa954543
babhero10 <abdallah.elsaid25@gmail.com>
2022-12-04 06:30:25 +0200

    config file change

--- src/backend/constants/FileConfig.java
+++
@@ -1,6 +0,0 @@
-package backend.constants;
-
-public interface FileConfig {
-    String VERSION = "2.2v";
-    long SERIAL_VERSION_UID = 8582036254333995277L;
-}
--- src/backend/constants/Properties.java
+++ src/backend/constants/Properties.java
@@ -2,7 +2,6 @@ package backend.constants;
 
 public interface Properties {
     String NAME_KEY = "name";
-    String PREV_NAME_KEY = "prev name";
     String SET_BORDER_KEY = "set border";
     String SET_FILL_KEY = "set fill";
 }
